7238980455bf3cb9f726ae7fd4cc68324107573b
Sail <wf.liu@aliyun.com>
2025-09-08 23:51:21 +0800

    wip

--- scripts/deployFundme.js
+++ scripts/deployFundme.js
@@ -1,4 +1,16 @@
+const { error } = require("console");
 const { ethers } = require("hardhat")
-function main (){
-    
+async function main (){
+    const fundMeFactory = await ethers.getContractFactory("fundMe");
+    const fundMe = await fundMeFactory.deploy();
+    await fundMe.waitForDeployment();
+    console.log(`Contract has been deployed successfully. Contract address: ${fundMe.target}`);
+
 }
+
+main()
+  .then(() => {})
+  .catch((error) => {
+    console.error('An error occurred:', error); 
+    process.exit(1);
+  });
