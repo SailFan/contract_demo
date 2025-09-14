116c23336709dfea7000a3f3141553a13578514a
Sail <wf.liu@aliyun.com>
2025-09-14 19:06:22 +0800

    wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,22 @@
-// hardhat.config.cjs
-// hardhat.config.cjs
+
+require("dotenv").config(); 
+require("@chainlink/env-enc").config();
+
 
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");   // 必须！
 
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 /** @type import('hardhat/types').Config */
-module.exports = {
+module.exports = { 
   solidity: "0.8.30",
 
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+      url: SEPOLIA_URL,
+      accounts: [ PRIVATE_KEY ],
     },
     hardhat: {
       // 使用默认设置
@@ -22,3 +27,12 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
+
+
+
+
+
+
+
+
+
--- scripts/deployFundme.js
+++ scripts/deployFundme.js
@@ -10,9 +10,15 @@ async function main(){
        }
         const fundMeFactory = await ethers.getContractFactory("FundMe")
         const fundMe = await fundMeFactory.deploy(10);
-        console.log("🚀 开始部署 FundMe 合约...")
+        console.log("🚀 开始部署 FundMe 合约...")  
         await fundMe.waitForDeployment();
         const contractAddress = await fundMe.getAddress();
+        await hre.run("verify:verify", {
+        address: fundMe.address,
+        constructorArguments: [10], // 如果有构造函数参数，在这里添加
+        });
+    console.log("✅ 合约验证成功!");
+    console.log("🌐 查看合约: https://sepolia.etherscan.io/address/" + fundMe.address);
         console.log("✅ 合约部署成功!");
         console.log(`📝 合约地址: ${contractAddress}`);
         console.log(`🔗 交易哈希: ${fundMe.deploymentTransaction().hash}`);
